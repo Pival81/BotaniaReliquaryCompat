eb1c36fbe8943499bd5bd42443af036124b2899a
Pival81 <pival801@gmail.com>
2022-10-24 23:56:56 +0200

    added mixin inject on onItemRightClick

--- src/main/java/org/pival81/botaniareliquarycompat/mixin/LilypadItemMixin.java
+++ src/main/java/org/pival81/botaniareliquarycompat/mixin/LilypadItemMixin.java
@@ -1,13 +1,31 @@
 package org.pival81.botaniareliquarycompat.mixin;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xreliquary.items.block.ItemFertileLilyPad;
+
+import javax.annotation.Nonnull;
 
 @Mixin(ItemFertileLilyPad.class)
 public class LilypadItemMixin {
 
     public boolean hasEffect(ItemStack stack){
         return true;
+    }
+
+    @Inject(at = @At("HEAD"), method = "onItemRightClick(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/util/EnumHand;)Lnet/minecraft/util/ActionResult;")
+    public void onItemRightClick(World world, EntityPlayer player, EnumHand hand, CallbackInfoReturnable<ActionResult<ItemStack>> cir) {
+        player.sendStatusMessage(new TextComponentString("Test test is this working?"), true);
     }
 }
